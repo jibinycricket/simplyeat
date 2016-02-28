78e7d23825c28c8be7cef0aa1f50bf30ac581e5c
jmathew1991 <jmathew1991@gmail.com>
2016-02-28 17:36:08 -0500

    Added Title Helper and Nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def setTitle(page_title = '')
+  default_title = "Menu Factory"
+  if (page_title == '')
+    return default_title;
+  else
+    return default_title + " | " + page_title;
+  end
+end
+
 end
