a0cae530278ed0a363a7e492409b4f6ec2914047
jmathew1991 <jmathew1991@gmail.com>
2016-03-02 20:28:17 -0500

    Added Modal and Styling

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -15,4 +15,29 @@
   }); // end DOM ready
 })(jQuery); // end jQuery
 
+/******** Registration/Login Modal ********/
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
+$(function(){
+  $('#login-modal #shared-login-link').remove();
+  $('#signup-modal #shared-signup-link').remove();
+});
+
+
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,24 @@
 module ApplicationHelper
 
-def setTitle(page_title = '')
-  default_title = "Menu Factory"
-  if (page_title == '')
-    return default_title;
-  else
-    return default_title + " | " + page_title;
+  def setTitle(page_title = '')
+    default_title = "Menu Factory"
+    if (page_title == '')
+      return default_title;
+    else
+      return default_title + " | " + page_title;
+    end
   end
-end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 
 end
