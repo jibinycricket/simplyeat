ca3ddd265550ce796d4ec278762fae908019daa1
jmathew1991 <jmathew1991@gmail.com>
2016-03-19 23:02:06 -0400

    Fixed AJAX issues

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,7 @@
 class Section < ActiveRecord::Base
   belongs_to :user
   has_many :items, dependent: :destroy
-  
+  validates :name, presence: true
   validates :user_id, presence: true
   default_scope{ order('created_at ASC')}
 end
